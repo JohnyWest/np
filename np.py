d93648131f54667383eff35ec08ece3a8e54b688
Johny_West <bbassed@gmail.com>
2023-03-26 02:46:13 +0500

    Added a file menu buttons

--- np.py
+++ np.py
@@ -5,8 +5,29 @@ root.title('Заметочки')
 root.geometry('700x800')
 root.iconbitmap('Ico.ico')
 
+#Раздел меню
+main_menu = Menu(root)
+
+#Файл
+file_menu = Menu(main_menu, tearoff=0)
+file_menu.add_command(label='Открыть')
+file_menu.add_command(label='Сохранить')
+file_menu.add_separator()
+file_menu.add_command(label='Закрыть')
+root.config(menu=main_menu)
+
+root.config(menu=main_menu)
+
+#Вид
+view_menu = Menu(main_menu, tearoff=0)
+view_menu.add_command(label='Открыть')
+
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
+
+#Добавление списков меню
+main_menu.add_cascade(label='Файл', menu=file_menu)
+
 
 #Settings of a text field
 text_field = Text(f_text, 
