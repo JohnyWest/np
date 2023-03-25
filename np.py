be4ac613bd69ed4a8965a4f72e77e009bd7e850d
Johny_West <bbassed@gmail.com>
2023-03-25 14:37:42 +0500

    Added a scrollbar and textfield settings

--- np.py
+++ np.py
@@ -8,8 +8,24 @@ root.iconbitmap('Ico.ico')
 f_text = Frame(root)
 f_text.pack(fill=BOTH, expand=1)
 
-text_field = Text(f_text, bg='gray', fg='white')
+#Settings of a text field
+text_field = Text(f_text, 
+                  bg='gray', 
+                  fg='white',
+                  padx=15,
+                  pady=15,
+                  wrap=WORD,
+                  insertbackground='brown',
+                  selectbackground='lime',
+                  spacing3=15,
+                  width=10
+                  )
 text_field.pack(expand=1, fill=BOTH, side='left')
+#Adding a scrollbar
+scroll = Scrollbar(f_text, command=text_field.yview)
+scroll.pack(side=LEFT, fill=Y)
+text_field.config(yscrollcommand=scroll.set)
 
 
+#Loop
 root.mainloop()
