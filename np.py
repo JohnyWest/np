142dedc4ba39513dd1729c5b31e04884438db9da
Johny_West <bbassed@gmail.com>
2023-03-26 02:56:44 +0500

    Added font menu and themes menu

--- np.py
+++ np.py
@@ -8,7 +8,7 @@ root.iconbitmap('Ico.ico')
 #Раздел меню
 main_menu = Menu(root)
 
-#Файл
+##Файл
 file_menu = Menu(main_menu, tearoff=0)
 file_menu.add_command(label='Открыть')
 file_menu.add_command(label='Сохранить')
@@ -18,16 +18,28 @@ root.config(menu=main_menu)
 
 root.config(menu=main_menu)
 
-#Вид
+##Вид
 view_menu = Menu(main_menu, tearoff=0)
-view_menu.add_command(label='Открыть')
+view_menu_sub = Menu(view_menu, tearoff=0)
+font_menu_sub = Menu(view_menu, tearoff=0)
+view_menu_sub.add_command(label='Темная тема')
+view_menu_sub.add_command(label='Светлая тема')
+view_menu.add_cascade(label='Тема', menu=view_menu_sub)
 
-f_text = Frame(root)
-f_text.pack(fill=BOTH, expand=1)
+###Шрифт
+font_menu_sub.add_command(label='Arial')
+font_menu_sub.add_command(label='Roboto')
+font_menu_sub.add_command(label='Times New Roman')
+view_menu.add_cascade(label='Шрифт', menu=font_menu_sub)
+root.config(menu=view_menu)
 
 #Добавление списков меню
 main_menu.add_cascade(label='Файл', menu=file_menu)
+main_menu.add_cascade(label='Вид', menu=view_menu)
 
+# Фрейм
+f_text = Frame(root)
+f_text.pack(fill=BOTH, expand=1)
 
 #Settings of a text field
 text_field = Text(f_text, 
